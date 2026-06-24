b4ce3cfe53c7110f93954209570fe58d2ae86702
roney ruiz <roneyruizrojas@gmail.com>
2026-06-24 14:12:00 -0600

    refac(infra): rename plugins to extensions

--- api/source/infrastructure/extensions/origins.extension.ts
+++ api/source/infrastructure/extensions/origins.extension.ts
@@ -1,7 +1,7 @@
 import { env } from "@env";
 import cors from "@elysia/cors";
 
-export const OriginsPlugin = cors({
+export const OriginsExtension = cors({
   origin: env.CORS_ORIGIN,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
