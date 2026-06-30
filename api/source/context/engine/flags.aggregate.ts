a15dc79ad56a7803abda8ac2d064faba02c02856
roney ruiz <roneyruizrojas@gmail.com>
2026-06-29 22:27:04 -0600

    chore(core): rename props to row properties

--- api/source/context/engine/flags.aggregate.ts
+++ api/source/context/engine/flags.aggregate.ts
@@ -2,7 +2,7 @@ import { FlagErrors, type FlagError } from "./flags.exceptions";
 import type { CreateAction, UpdateAction } from "./flags.interfaces";
 import { Success, Failure, type Result } from "@shared/helpers/result.helper";
 
-interface Props {
+interface Row {
   id: string;
   key: string;
   name: string;
@@ -12,33 +12,33 @@ interface Props {
 };
 
 export class Flag {
-  private constructor(private props: Props) { };
+  private constructor(private row: Row) { };
 
   public enable(): void {
-    this.props.enabled = true;
+    this.row.enabled = true;
   };
 
   public disable(): void {
-    this.props.enabled = false;
+    this.row.enabled = false;
   };
 
-  public primitives(): Props {
-    return structuredClone(this.props);
+  public primitives(): Row {
+    return structuredClone(this.row);
   };
 
-  public static hydrate(props: Props): Flag {
+  public static hydrate(props: Row): Flag {
     return new Flag(props)
   };
 
-  get id() { return this.props.id; }
-  get key() { return this.props.key; }
-  get name() { return this.props.name; }
-  get short() { return this.props.short; }
-  get enabled() { return this.props.enabled; }
-  get default() { return this.props.default; }
+  get id() { return this.row.id; }
+  get key() { return this.row.key; }
+  get name() { return this.row.name; }
+  get short() { return this.row.short; }
+  get enabled() { return this.row.enabled; }
+  get default() { return this.row.default; }
 
 
-  public update(cmd: UpdateAction): Result<void, FlagError> {
+  public refresh(cmd: UpdateAction): Result<void, FlagError> {
     if (cmd.name !== undefined && cmd.name.trim().length === 0) {
       return Failure(FlagErrors.invalidName(cmd.name));
     };
@@ -47,15 +47,15 @@ export class Flag {
       return Failure(FlagErrors.invalidShort(cmd.short));
     };
 
-    if (cmd.name !== undefined) this.props.name = cmd.name;
-    if (cmd.short !== undefined) this.props.short = cmd.short;
-    if (cmd.default !== undefined) this.props.default = cmd.default;
+    if (cmd.name !== undefined) this.row.name = cmd.name;
+    if (cmd.short !== undefined) this.row.short = cmd.short;
+    if (cmd.default !== undefined) this.row.default = cmd.default;
 
     return Success(undefined);
   };
 
 
-  public static create(cmd: CreateAction): Result<Flag, FlagError> {
+  public static factory(cmd: CreateAction): Result<Flag, FlagError> {
     const isValidKey = /^[a-z0-9]+(-[a-z0-9]+)*$/.test(cmd.key);
     if (!isValidKey) return Failure(FlagErrors.invalidKey(cmd.key));
 
